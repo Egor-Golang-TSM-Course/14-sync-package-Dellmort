97170b8b56111ae040e50d195ae8ec572e4587dc
Dellmort <shatowkikree@mail.ru>
2023-12-17 19:27:57 +0300

    add task4

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"lesson14/task1"
-	"lesson14/task2"
-	"lesson14/task3"
+	// "lesson14/task1"
+	// "lesson14/task2"
+	// "lesson14/task3"
+	"lesson14/task4"
 )
 
 func main() {
-	task1.Start()
-	task2.Start()
-
-	task3.Start()
+	// task1.Start()
+	// task2.Start()
+	// task3.Start()
+	task4.Start()
 }
